0d7a660a11e963635774f5f2bbe115221b52a9e8
SergeySN84 <Sergsvircuk@mail.ru>
2026-01-15 23:31:42 +0700

    Создание деплоя на сервер.

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -39,7 +39,7 @@ class Migration(migrations.Migration):
                     models.BooleanField(
                         default=False,
                         help_text="Designates that this user has all "
-                                  "permissions without explicitly assigning them.",
+                        "permissions without explicitly assigning them.",
                         verbose_name="superuser status",
                     ),
                 ),
@@ -50,7 +50,7 @@ class Migration(migrations.Migration):
                             "unique": "A user with that username already exists."
                         },
                         help_text="Required. 150 characters or fewer."
-                                  " Letters, digits and @/./+/-/_ only.",
+                        " Letters, digits and @/./+/-/_ only.",
                         max_length=150,
                         unique=True,
                         validators=[
@@ -76,7 +76,7 @@ class Migration(migrations.Migration):
                     models.BooleanField(
                         default=False,
                         help_text="Designates whether the user "
-                                  "can log into this admin site.",
+                        "can log into this admin site.",
                         verbose_name="staff status",
                     ),
                 ),
@@ -85,8 +85,8 @@ class Migration(migrations.Migration):
                     models.BooleanField(
                         default=True,
                         help_text="Designates whether this user should be"
-                                  " treated as active. Unselect this instead "
-                                  "of deleting accounts.",
+                        " treated as active. Unselect this instead "
+                        "of deleting accounts.",
                         verbose_name="active",
                     ),
                 ),
@@ -102,8 +102,8 @@ class Migration(migrations.Migration):
                     models.ManyToManyField(
                         blank=True,
                         help_text="The groups this user belongs to."
-                                  " A user will get all permissions granted "
-                                  "to each of their groups.",
+                        " A user will get all permissions granted "
+                        "to each of their groups.",
                         related_name="user_set",
                         related_query_name="user",
                         to="auth.group",
