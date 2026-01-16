d0cf7002d7ac18163521773e810b602e1fd095f9
SergeySN84 <Sergsvircuk@mail.ru>
2026-01-16 20:32:21 +0700

    Внесение изменений в проект.

--- api/settings.py
+++ api/settings.py
@@ -35,8 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-if not SECRET_KEY:
-    raise ImproperlyConfigured("SECRET_KEY is required")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
