2541dce4eeae75cc98f23866ce615a822f6934a3
SergeySN84 <Sergsvircuk@mail.ru>
2026-01-16 21:49:58 +0700

    Внесение изменений в проект.

--- users/serializers.py
+++ users/serializers.py
@@ -1,16 +1,18 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import User
 
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ["id", "username", "email", "is_staff"]
-        read_only_fields = ["is_staff"]
+        fields = ("id", "username", "email", "password")
 
     def create(self, validated_data):
         password = validated_data.pop("password")
-        user = User(**validated_data)
+        user = User.objects.create(**validated_data)
         user.set_password(password)
         user.save()
         return user
