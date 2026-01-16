8f5d24bc1229fccc5140e41d6c1bf571f1a83fe2
SergeySN84 <Sergsvircuk@mail.ru>
2026-01-16 21:24:00 +0700

    Внесение изменений в проект.

--- api/settings.py
+++ api/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 import sys
 
-# from django.core.exceptions import ImproperlyConfigured
+from django.core.exceptions import ImproperlyConfigured
 
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 from pathlib import Path
 from datetime import timedelta
 
@@ -23,12 +23,13 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# if os.getenv("DOCKER_ENV"):
-#     env_path = BASE_DIR / ".env"
-# else:
-#     env_path = BASE_DIR / ".env.local"
-#
-# load_dotenv(env_path)
+
+if os.getenv("DOCKER_ENV"):
+    env_path = BASE_DIR / ".env"
+else:
+    env_path = BASE_DIR / ".env.local"
+
+load_dotenv(env_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -38,9 +39,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
-
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
